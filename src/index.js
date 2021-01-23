31e6f448f6fd1c6eaeffb6887ecda509cd9d21e1
Bilaal Ahmad <bilaal-ahmad@hotmail.co.uk>
2021-01-23 18:44:00 +0000

    instantiated ApolloClient wrapped app with ApolloProvider ran GraphQL test query

--- src/index.js
+++ src/index.js
@@ -4,18 +4,49 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient, gql } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
 
+// ApolloClient Config
+const httpLink = createHttpLink({ uri: 'https://crwn-clothing.com' });
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+// Test Query
+client
+  .query({
+    query: gql`
+      query {
+        collections {
+          id
+          title
+        }
+      }
+    `,
+  })
+  .then((result) => console.log('collectionsQuery', result))
+  .catch((err) => console.log(err));
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
